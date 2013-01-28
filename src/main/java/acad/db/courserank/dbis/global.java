d5e277b02c719821d455ef80fe6e1f938dda6a3e
Rishabh Singhal <rinkusinghal05@gmail.com>
2013-01-28 22:54:11 +0530

    password for db changed

--- src/main/java/acad/db/courserank/dbis/global.java
+++ src/main/java/acad/db/courserank/dbis/global.java
@@ -12,7 +12,7 @@ public class global {
     public static String driver = "com.mysql.jdbc.Driver";
     public static String url = "jdbc:mysql://127.9.63.129:3306/courserank";
     public static String username = "admin";
-    public static String password ="vcUsYACtQmUE";
+    public static String password ="rinku";
     //public static String url = "jdbc:mysql://localhost:3306/dbis";
     //public static String username = "root";
     //public static String password ="rinku";
